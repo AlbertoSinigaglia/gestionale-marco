f052c7cd4f855f65e3734aa41391a4160c627926
alberto <sinigagliaalberto@gmail.com>
2020-10-31 12:27:09 +0100

    test middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,17 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV') === 'production') {
-            $trustedHeaders = [
-                Request::HEADER_X_FORWARDED_FOR,
-                Request::HEADER_X_FORWARDED_PROTO,
-                Request::HEADER_X_FORWARDED_PORT,
-            ];
-            Request::setTrustedProxies(
-                array($_SERVER['REMOTE_ADDR']),
-                array_reduce($trustedHeaders, function($carry, $value) {
-                    return $carry ^ $value;
-                }, 0)
-            );
+            $request = Request::createFromGlobals();
+
+            Request::setTrustedProxies([$request->server->get('REMOTE_ADDR')], Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
