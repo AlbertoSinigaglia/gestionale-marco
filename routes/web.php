1cc0ab0a2dc63664269906b41aa1bc73e45c9deb
alberto <sinigagliaalberto@gmail.com>
2020-10-31 02:51:56 +0100

    fix route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
